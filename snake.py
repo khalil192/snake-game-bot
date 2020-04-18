4e0c2887b7e2ed5d9490244d54bed5a870a116e1
guru kiran <47276342+gurukiran07@users.noreply.github.com>
2020-04-18 18:42:36 +0530

    Add files via upload

--- snake.py
+++ snake.py
@@ -6,6 +6,7 @@ class Snake:
         self.snake_length=snake_length
         self.snake_x=snake_x
         self.snake_y=snake_y
+        self.is_alive=True
         if self.snake_length>1:
             self.snake_lst=[]
             for i in range(self.snake_length):
@@ -15,8 +16,8 @@ class Snake:
         else:
             self.snake_lst=[[self.snake_x,self.snake_y]]
         self.snake_sx=0
-        self.snkae_sy=0
-        self.direction={'l':False,'r':False,'u':False,'d':False}
+        self.snake_sy=0
+        self.direction={'l':False,'r':True,'u':False,'d':False}
     def PlotSnake(self,GameDisplay):
         for x,y in self.snake_lst:
             GameDisplay.blit(self.img,(x*20,y*20))
@@ -29,6 +30,46 @@ class Snake:
     def Check(self,x,y):
         return [x,y] in self.snake_lst
 
+    def Move(self,move,rows):
+        if move=='l' and self.direction['r']==False:
+            self.direction['l']=True
+            self.direction['u']=False
+            self.direction['d']=False
+            self.snake_sx=-1
+            self.snake_sy=0
+        elif move=='r' and self.direction['l']==False:
+            self.direction['r']=True
+            self.direction['u']=False
+            self.direction['d']=False
+            self.snake_sx=1
+            self.snake_sy=0
+        elif move=='u' and self.direction['d']==False:
+            self.direction['u']=True
+            self.direction['l']=False
+            self.direction['r']=False
+            self.snake_sy= -1
+            self.snake_sx=0
+        elif move=='d' and self.direction['u']==False:
+            self.direction['d']=True
+            self.direction['l']=False
+            self.direction['r']=False
+            self.snake_sy=1
+            self.snake_sx=0
+        self.snake_x+=self.snake_sx
+        self.snake_y+=self.snake_sy
+        if self.snake_x<0 or self.snake_x>rows-1 or self.snake_y<0 or self.snake_y>rows-1:
+            self.is_alive=False
+        else:
+            if (self.snake_x,self.snake_y) in self.snake_lst[:-1]:
+                self.is_alive=False
+            if not (self.snake_x,self.snake_y) == self.snake_lst[-1]:
+                self.snake_lst.append((self.snake_x,self.snake_y))
+            if len(self.snake_lst)>self.snake_length:
+                del self.snake_lst[0]
+        
+
+
 if __name__=='__main__':
     snake=Snake(r'snakeBody.png',3,3,0)
+
     print(snake.GetData())
